8bd76e4c1c7c1b80ca16c4a05f8a45e0d50fb70e
Abid Hasan <43554764+abidtkg@users.noreply.github.com>
2019-04-18 02:56:50 +0600

    update

--- version/js-only/JS-Only-1.5/assets/js/main-script.js
+++ version/js-only/JS-Only-1.5/assets/js/main-script.js
@@ -33,17 +33,25 @@ if(day == "Sunday"){
     document.getElementById("classOneTime").innerHTML = "11:25 am > 12:45 pm";
     document.getElementById("classTwoTime").innerHTML = "12:45 pm > 10:25 pm";
     document.getElementById("classThreeTime").innerHTML = "01:25 pm > 02:05 pm";
+    document.getElementById("forthsub").innerHTML = "4";
 }else{
     if(day == "Monday"){
         document.getElementById("preOne").innerHTML = "Principal Of Digital Electornics";
         document.getElementById("preTwo").innerHTML = "Computer Periferrls";
         document.getElementById("preThree").innerHTML = "Business & Organization";
         document.getElementById("preFour").innerHTML = "Data Stucture & Algorithms";
+        document.getElementById("classOneTime").innerHTML = "11:25 am > 12:05 pm";
+        document.getElementById("classTwoTime").innerHTML = "12:05 pm > 01:25 pm";
+        document.getElementById("classThreeTime").innerHTML = "01:25 pm > 02:05 pm";
+        document.getElementById("classFourTime").innerHTML = "02:05 pm > 02:45 pm";
     }else{
         if(day == "Tuesday"){
             document.getElementById("preOne").innerHTML = "Data Communication";
             document.getElementById("preTwo").innerHTML = "Object Oriented Programming";
             document.getElementById("preThree").innerHTML = "Principal Of Digital Electornics";
+            document.getElementById("classOneTime").innerHTML = "11:25 am > 12:45 pm";
+            document.getElementById("classTwoTime").innerHTML = "12:45 pm > 01:25 pm";
+            document.getElementById("classThreeTime").innerHTML = "01:25 pm > 02:45 pm";
         }else{
             if(day == "Wednesday"){
                 document.getElementById("preOne").innerHTML = "Web Development";
@@ -57,18 +65,24 @@ if(day == "Sunday"){
                     document.getElementById("preOne").innerHTML = "Data Communication";
                     document.getElementById("preTwo").innerHTML = "Principal Of Digital Electornics";
                     document.getElementById("preThree").innerHTML = "Data Communication";
+                    document.getElementById("classOneTime").innerHTML = "11:25 am > 12:45 pm";
+                    document.getElementById("classTwoTime").innerHTML = "12:45 pm > 01:25 pm";
+                    document.getElementById("classThreeTime").innerHTML = "01:25 pm > 02:45 pm";
                 }else{
                     if(day == "Friday"){
                         document.getElementById("preOne").innerHTML = "Holiday";
                         document.getElementById("preTwo").innerHTML = "Holiday";
                         document.getElementById("preThree").innerHTML = "Holiday";
                         document.getElementById("preFour").innerHTML = "Holiday";
+                        document.getElementById("forthsub").innerHTML = "4";
                     }else{
                         if(day == "Saturday"){
                             document.getElementById("preOne").innerHTML = "Principal Of Digital Electornics";
                             document.getElementById("preTwo").innerHTML = "Object Oriented Programming";
                             document.getElementById("preThree").innerHTML = "Data Structure	& Algorithms";
                             document.getElementById("preFour").innerHTML = "Computer Periferrerls";
+                        }else{
+                            document.getElementById("week").innerHTML = "There is some error";
                         }
                     }
                 }
